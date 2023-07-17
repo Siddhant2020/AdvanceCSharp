00b064becca0596dafb5c861270510e0b078b7ec
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2023-07-17 08:56:36 +0530

    interface added

---
+++ Interfaces/DemoLibrary/DigitalProductModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    internal class DigitalProductModel
+    {
+        public string Title { get; set; }
+        public bool HasOrderBeenCompleted { get; private set; }
+        public void ShipItem(CustomerModel customer)
+        {
+            if (HasOrderBeenCompleted == false)
+            {
+                Console.WriteLine($"Simulating shipping {Title} to {customer.EmailAddress}");
+                HasOrderBeenCompleted = true;
+            }
+        }
+    }
+}
